66ea9aedd14506b35df0f4c3ab287ba196819acc
Jeremiah Orem <jeremy.orem@gmail.com>
2012-12-21 11:12:05 -0800

    Use dj-database-url for database settings.

--- sites/dev/settings.py
+++ sites/dev/settings.py
@@ -1,23 +1,19 @@
+import dj_database_url
+
 import private
 from gelato.admin.settings import *
 
-HOSTNAME='marketplace-admin-dev.allizom.org'
-DOMAIN=HOSTNAME
+HOSTNAME = 'marketplace-admin-dev.allizom.org'
+DOMAIN = HOSTNAME
 SITE_URL = 'http://%s' % DOMAIN
 STATIC_URL = SITE_URL
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'mysql_pymysql',
-        'NAME': private.DATABASES_DEFAULT_NAME,
-        'HOST': private.DATABASES_DEFAULT_HOST,
-        'PORT': private.DATABASES_DEFAULT_PORT,
-        'USER': private.DATABASES_DEFAULT_USER,
-        'PASSWORD': private.DATABASES_DEFAULT_PASSWORD,
-        'OPTIONS': {'init_command': 'SET storage_engine=InnoDB'},
-        'TEST_CHARSET': 'utf8',
-        'TEST_COLLATION': 'utf8_general_ci',
-    },
-}
+DATABASES = {'default': dj_database_url.parse(private.DATABASES_DEFAULT_URL)}
+DATABASES.update({
+    'ENGINE': 'mysql_pymysql',
+    'OPTIONS': {'init_command': 'SET storage_engine=InnoDB'},
+    'TEST_CHARSET': 'utf8',
+    'TEST_COLLATION': 'utf8_general_ci',
+})
 
 SESSION_COOKIE_DOMAIN = ".%s" % HOSTNAME
